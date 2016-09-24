cc1c71571fe397f4c7d40de42d18f20acc3c8319
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-09-24 17:04:15 +0200

    Implement the Add procedure to collect statistics

---
+++ src/etherscope-stats.adb
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------
+--  etherscope-stats -- Ethernet Packet Statistics
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+package body EtherScope.Stats is
+
+   --  ------------------------------
+   --  Update the statistics after reception of a packet of the given length.
+   --  ------------------------------
+   procedure Add (Stats  : in out Statistics;
+                  Length : in Net.Uint32) is
+      use type Net.Uint32;
+   begin
+      Stats.Packets := Stats.Packets + 1;
+      Stats.Bytes   := Stats.Bytes + Length;
+   end Add;
+
+end EtherScope.Stats;
