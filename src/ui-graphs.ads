721ae198d466e9d8766a09ca6b504a2e9956b572
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-06-01 21:56:45 +0200

    Update to use the new HAL.Bitmap interface

--- src/ui-graphs.ads
+++ src/ui-graphs.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  ui-graphs -- Generic package to draw graphs
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,6 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 with HAL.Bitmap;
-with Bitmapped_Drawing;
 with Ada.Real_Time;
 generic
    type Value_Type is mod <>;
@@ -27,7 +26,7 @@ package UI.Graphs is
 
    type Graph_Type is limited record
       Rate             : Ada.Real_Time.Time_Span;
-      Pos              : Bitmapped_Drawing.Point;
+      Pos              : HAL.Bitmap.Point;
       Width            : Natural;
       Height           : Natural;
       Current_Sample   : Value_Type;
@@ -60,7 +59,7 @@ package UI.Graphs is
    function Compute_Max_Value (Graph : in Graph_Type) return Value_Type;
 
    --  Draw the graph.
-   procedure Draw (Buffer : in HAL.Bitmap.Bitmap_Buffer'Class;
+   procedure Draw (Buffer : in out HAL.Bitmap.Bitmap_Buffer'Class;
                    Graph  : in out Graph_Type);
 
 end UI.Graphs;
